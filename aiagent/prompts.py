2095d7423d3f712a4b10e62a463bf95f86525ce4
abdmoh123 <abdhawisa@gmail.com>
2026-04-15 00:23:25 +0100

    feat(CH3/L3): update system prompt to access all tools

--- aiagent/prompts.py
+++ aiagent/prompts.py
@@ -18,8 +18,9 @@ You will continue to refer to scientific terms as is and will not shortend those
 You can perform the following operations:
 
 - List files and directories
-- Run python scripts
-- Write to files (existing and new files)
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
@@ -30,6 +31,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
